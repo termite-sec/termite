65477404ae97acfb385bd6f1626651418576dfe6
VarshaDanduri <varshadanduri@gmail.com>
2026-04-14 17:13:31 +0000

    Add new kitin

--- cmd/dig/main.go
+++ cmd/dig/main.go
@@ -526,11 +526,14 @@ func sendScan(token string, files []File, modes []string) ([]Finding, error) {
 
 func printHelp() {
 	logo := rustStyle.Render(`
- | |                    (_) |
- | |_ ___ _ __ _ __ ___  _| |____
- | __/ _ \ '__| '_ ` + "`" + ` _ \
- | ||  __/ |  | | | | | | | || __/
-  \__\___|_|  |_| |_| |_|_|\_\___|`)
+  _    _ _   _       
+ | |  (_) | (_)      
+ | | ___| |_ _ _ __  
+ | |/ / | __| | '_ \ 
+ |   <| | |_| | | | |
+ |_|\_\_|\__|_|_| |_|
+                     
+                     `)
 
 	bug := soilStyle.Render(`
        --  \
